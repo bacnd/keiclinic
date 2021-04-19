0c027e9efdf466da963af282ae513c2803caf65c
bacnd <nguyendinhbac99@gmail.com>
2021-04-19 12:12:47 +0700

    Code top page

--- gulpfile.js
+++ gulpfile.js
@@ -24,34 +24,42 @@ const cleanFonts = () => {
   return del(['dist/assets/fonts']);
 }
 
+const cleanStyles = () => {
+  return del(['dist/assets/css']);
+}
+
+const cleanJavascripts = () => {
+  return del(['dist/assets/js']);
+}
+
 // css task
 const css = () => {
-  return src('src/scss/styles.scss')
+  return src('src/assets/scss/styles.scss')
     .pipe(mode.development( sourcemaps.init() ))
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(rename('styles.css'))
     .pipe(mode.production( csso() ))
     .pipe(mode.development( sourcemaps.write() ))
-    .pipe(dest('dist'))
+    .pipe(dest('src/assets/css/'))
     .pipe(mode.development( browserSync.stream() ));
 }
 
 // js task
 const js = () => {
-  return src('src/**/*.js')
+  return src('src/assets/**/*.js')
     .pipe(babel({
       presets: ['@babel/env']
     }))
-    .pipe(webpack({
-      mode: 'development',
-      devtool: 'inline-source-map'
-    }))
-    .pipe(mode.development( sourcemaps.init({ loadMaps: true }) ))
-    .pipe(rename('app.js'))
-    .pipe(mode.production( terser({ output: { comments: false }}) ))
-    .pipe(mode.development( sourcemaps.write() ))
-    .pipe(dest('dist'))
+    // .pipe(webpack({
+    //   mode: 'development',
+    //   devtool: 'inline-source-map'
+    // }))
+    // .pipe(mode.development( sourcemaps.init({ loadMaps: true }) ))
+    // .pipe(rename('app.js'))
+    // .pipe(mode.production( terser({ output: { comments: false }}) ))
+    // .pipe(mode.development( sourcemaps.write() ))
+    // .pipe(dest('src/assets/js/'))
     .pipe(mode.development( browserSync.stream() ));
 }
 
@@ -66,16 +74,37 @@ const copyFonts = () => {
     .pipe(dest('dist/assets/fonts'));
 }
 
+const copyJavascripts = () => {
+  return src('src/assets/js/**/*.js')
+    .pipe(dest('dist/assets/js'));
+}
+
+const copyStyles = () => {
+  return src('src/assets/css/**/*.css')
+    .pipe(dest('dist/assets/css'));
+}
+
+const copyHtmls = () => {
+  return src('src/**/*.html')
+    .pipe(dest('./'));
+}
+
 // watch task
 const watchForChanges = () => {
   browserSync.init({
     server: {
-      baseDir: './'
+      proxy:"localhost:8010",
+      baseDir: './src/',
+      https: true,
+      ghostMode: {
+          scroll: true
+      },
+      scrollProportionally: true,
     }
   });
 
-  watch('src/scss/**/*.scss', css);
-  watch('src/**/*.js', js);
+  watch('src/assets/scss/**/*.scss', css);
+  watch('src/assets/**/*.js', js);
   watch('**/*.html').on('change', browserSync.reload);
   watch('src/assets/images/**/*.{png,jpg,jpeg,gif,svg}', series(cleanImages, copyImages));
   watch('src/assets/fonts/**/*.{svg,eot,ttf,woff,woff2}', series(cleanFonts, copyFonts));
@@ -83,4 +112,4 @@ const watchForChanges = () => {
 
 // public tasks
 exports.default = series(clean, parallel(css, js, copyImages, copyFonts), watchForChanges);
-exports.build = series(clean, parallel(css, js, copyImages, copyFonts));
+exports.build = series(clean, parallel(css, js, copyImages, copyFonts, copyJavascripts, copyStyles, copyHtmls));
---
+++ src/assets/js/common.js
@@ -0,0 +1,51 @@
+$(function() {
+    var pageTop = $('#page_top');
+    pageTop.hide();
+  
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 500) {
+            pageTop.fadeIn('slow');
+        } else {
+            pageTop.fadeOut('slow');
+        }
+    });
+
+    pageTop.click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+    $('.slide_box').slick({
+        autoplay: true,
+        autoplaySpeed: 7000,
+        dots: false,
+        arrows: false,
+        fade: true,
+        cssEase: 'ease-out',
+        speed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false
+    });
+});
+
+function initMap() {
+    const bangalore = { lat: 34.96725, lng: 137.16321 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: bangalore,
+    });
+    const image = window.location.origin + "/assets/images/marker-icon.png";
+    // Add a marker at the center of the map.
+    addMarker(bangalore, map, image);
+}
+
+// Adds a marker to the map.
+function addMarker(location, map, image) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    new google.maps.Marker({
+        position: location,
+        map: map,
+        icon: image
+    });
+}
